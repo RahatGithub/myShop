4fdddfc72feee2bfc4b10f70984eae54abbf0237
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2021-11-04 20:04:32 +0600

    First slider completed

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
+from math import ceil
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+
+    products = Product.objects.all()
+
+    n = len(products)
+
+    num_of_slide = ceil(n/4)
+
+    params = {'num_of_slide': num_of_slide, 'range': range(1,num_of_slide), 'product': products}
+
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
